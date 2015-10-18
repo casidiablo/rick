5b86f88fc4ec29ff2439d0157cc5303e5e7acbef
Cristian <me@cristian.io>
2015-10-18 15:22:52 -0700

    Updated clj-slack to 0.5.1

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [slack-rtm "0.1.0"]
-                 [org.julienxx/clj-slack "0.5.0"]
+                 [org.julienxx/clj-slack "0.5.1"]
                  [environ "1.0.1"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/rick/core.clj
+++ src/rick/core.clj
@@ -1,15 +1,14 @@
 (ns rick.core
   (:require [slack-rtm.core :as rtm]
             [clj-slack.chat :as chat]
+            [clj-slack.reactions :as reactions]
             [clojure.core.async :refer [chan <!!]]
-            [clj-slack.core :refer [slack-request stringify-keys]]
             [environ.core :refer [env]]))
 
 (def token (env :rick-token))
 (def target-user (env :rick-user))
 (def target-channel (env :rick-channel))
 (def reaction-emoji (or (env :rick-reaction) "white_check_mark"))
-
 (def connection {:api-url "https://slack.com/api" :token token})
 
 (defn on-reaction-removed [{:keys [user item reaction] :as msg}]
@@ -18,18 +17,9 @@
            (= (:channel item) target-channel))
     (chat/delete connection (:ts item) target-channel)))
 
-
-(defn add-reaction
-  "Add a checkmark reaction to the provided message"
-  [optionals]
-  (->> optionals
-       stringify-keys
-       (merge {"name" reaction-emoji})
-       (slack-request connection "reactions.add")))
-
 (defn on-message-received [{:keys [user channel ts] :as msg}]
   (if (and (= user target-user) (= channel target-channel))
-    (add-reaction {:channel channel :timestamp ts})))
+    (reactions/add connection reaction-emoji {:channel channel :timestamp ts})))
 
 (defn -main [& args]
   (let [rtm-conn (rtm/connect token)
